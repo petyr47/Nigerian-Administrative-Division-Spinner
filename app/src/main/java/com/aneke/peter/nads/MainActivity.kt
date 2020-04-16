9e756f03496a8522e5657656ecf6ee725e3363f8
petyr47 <anekepeter47@gmail.com>
2020-04-17 00:52:36 +0100

    complete sample app

--- app/src/main/java/com/aneke/peter/nads/MainActivity.kt
+++ app/src/main/java/com/aneke/peter/nads/MainActivity.kt
@@ -2,18 +2,44 @@ package com.aneke.peter.nads
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.aneke.peter.nads.databinding.ActivityMainBinding
+import com.aneke.peter.nigerianadministrativedivisionspinner.getAllStates
 import com.aneke.peter.nigerianadministrativedivisionspinner.models.State
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        lga.setCurrentState(State(name = "Lagos"))
+        val viewModel : MainViewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        binding.lifecycleOwner = this
+        binding.viewModel = viewModel
+
+//        viewModel.state.observe(this, Observer {
+//            it?.let {
+//                lga.setCurrentState(it)
+//            }
+//        })
+
+
+        state.setOnItemClickListener { _, _, _, id ->
+            lga.setCurrentState(getAllStates()[id.toInt()].name)
+        }
+
+
+
+
 
 
 
--- app/src/main/java/com/aneke/peter/nads/MainViewModel.kt
+++ app/src/main/java/com/aneke/peter/nads/MainViewModel.kt
@@ -1,6 +1,9 @@
 package com.aneke.peter.nads
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MainViewModel : ViewModel() {
+
+    val state = MutableLiveData<String>()
 }
